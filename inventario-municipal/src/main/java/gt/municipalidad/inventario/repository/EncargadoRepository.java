9dc189ab4b4912ace27336c06a3b4647a293f878
Byron <79301316+Byronsalguero@users.noreply.github.com>
2026-08-19 15:33:03 -0600

    Agregar repositorio de encargados

---
+++ inventario-municipal/src/main/java/gt/municipalidad/inventario/repository/EncargadoRepository.java
@@ -0,0 +1,4 @@
+package gt.municipalidad.inventario.repository;
+import gt.municipalidad.inventario.model.Encargado;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EncargadoRepository extends JpaRepository<Encargado, Long> {}
